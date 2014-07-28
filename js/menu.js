d4c347dcaacfc6bc162bb93c4cd9dca705e6f8bf
gianci15 <gianci15@ingenieros.com>
2014-07-28 13:13:56 -0500

    Cronofire Terminado V1.0

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,21 @@
 var inicio = $("#drawer");
 var menutemp = $("#menutemp");
 var menucrono = $("#menucrono");
+var menuacerca = $("#menuacerca");
 inicio.html(menucrono);	
 $(function(){ 
 
 $("#crono").click(crono);
 $("#temp").click(temp);
+$("#acerca").click(acerca);
 
 function crono(){
 inicio.html(menucrono);
 }
 function temp(){
 inicio.html(menutemp);
+}
+function acerca(){
+inicio.html(menuacerca);
 }
 })
